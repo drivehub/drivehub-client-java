89d0f209e0c615a05ce6f18f9cec0c8101214bbf
Igor Russkih <irusskih@gmail.com>
2010-07-13 16:22:20 +0400

    tabs to spaces!

--- src/drivehub/client/MemoryRecordStore.java
+++ src/drivehub/client/MemoryRecordStore.java
@@ -5,49 +5,49 @@ import java.util.Vector;
 
 public class MemoryRecordStore implements SensorRecordStore {
 
-	Vector recordStore = new Vector();
-	
-	@Override
-	public void deleteRecord(int id) {
-		// TODO Auto-generated method stub
+    Vector recordStore = new Vector();
+    
+    @Override
+    public void deleteRecord(int id) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public Enumeration enumerateRecordIDs() {
-		return new Enumeration() {
-			int idx = 0;
-			@Override
-			public boolean hasMoreElements() {
-				return recordStore.size() > idx;
-			}
+    @Override
+    public Enumeration enumerateRecordIDs() {
+        return new Enumeration() {
+            int idx = 0;
+            @Override
+            public boolean hasMoreElements() {
+                return recordStore.size() > idx;
+            }
 
-			@Override
-			public Object nextElement() {
-				if (recordStore.size() <= idx) return null;
-				Integer id = new Integer(idx);
-				idx++;
-				return id;
-			}
-		};
-	}
+            @Override
+            public Object nextElement() {
+                if (recordStore.size() <= idx) return null;
+                Integer id = new Integer(idx);
+                idx++;
+                return id;
+            }
+        };
+    }
 
-	@Override
-	public byte[] getRecord(int id) {
-		if (recordStore.size() <= id) return null;
-		return (byte[])recordStore.elementAt(id);
-	}
+    @Override
+    public byte[] getRecord(int id) {
+        if (recordStore.size() <= id) return null;
+        return (byte[])recordStore.elementAt(id);
+    }
 
-	@Override
-	public void storeRecord(byte[] record, int size) {
-		System.out.println("STORING "+size);
-		byte[] record_dup = new byte[size];
-		System.arraycopy(record, 0, record_dup, 0, size);
-		recordStore.add(record_dup);
-	}
-	
-	public void clear() {
-		recordStore.clear();
-	}
+    @Override
+    public void storeRecord(byte[] record, int size) {
+        System.out.println("STORING "+size);
+        byte[] record_dup = new byte[size];
+        System.arraycopy(record, 0, record_dup, 0, size);
+        recordStore.add(record_dup);
+    }
+    
+    public void clear() {
+        recordStore.clear();
+    }
 
 }
